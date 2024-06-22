652fe8e9225edb332cc0cf77f4f257b64613dd2f
Rishu <royadma0@gmail.com>
2024-06-22 21:13:02 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+export default class SolarisAI {
+    /**
+     * Create a new SolarisAI Instance
+     * 
+     * @param {string} api_key The api key that was provided
+     */
+    constructor(api_key) {
+        this.apiKey = api_key
+        if (apiKey == undefined || apiKey == null || apiKey == "" || typeof apiKey !== "string") this.apiKey = "";
+    }
+    
+}
